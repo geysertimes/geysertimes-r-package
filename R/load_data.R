136f0ed7388ad5b118211767ff264b3738cd3400
codemasta14 <codemasta14@gmail.com>
2019-05-02 17:06:15 -0600

    small update to load_data function.

--- R/load_data.R
+++ R/load_data.R
@@ -5,7 +5,7 @@ load_data <- function(){
   
   
   if(Sys.getenv("GeyseRdataPath")==""){
-    path <- ifelse(readline("Would you like to set a custom path to set the data? \n Y/N \n ") %in% c("y","Y"),
+    path <- ifelse(readline("Would you like to set a custom path to save the data? \n Y/N \n ") %in% c("y","Y"),
                    paste0(readline("Please enter a path \n "),"/GeyseRdata_",Sys.Date(),".tsv"),
                    rappdirs::user_data_dir(paste0("GeyseRdata_",Sys.Date(),".tsv"),"GeyseRdata"))
   }
@@ -16,7 +16,7 @@ load_data <- function(){
   if(regmatches(time_dif,regexpr("\\d(?=\\s)",time_dif,perl = T)) != "0") {
     if(readline(paste0("Your current file is ",time_dif," old. Would you like a newer version? \n Y/N \n ")) %in% c("y","Y")){
       wow <- tempfile()
-      downloader::download(paste0("https://geysertimes.org/archive/complete/geysertimes_eruptions_complete_",Sys.Date(),".tsv.gz"),wow)
+      download.file(paste0("https://geysertimes.org/archive/complete/geysertimes_eruptions_complete_",Sys.Date(),".tsv.gz"),wow)
       R.utils::gunzip(wow,destname = path,remove = T,overwrite = T)
       print(paste("File Saved to",path))
     }
