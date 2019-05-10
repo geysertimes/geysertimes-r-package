4e05c912ee5aab95483004c98f1bd7f3ce591933
spkaluzny <spkaluzny@gmail.com>
2019-05-10 16:58:37 -0700

    Move all files to geysertimes-r-package

---
+++ R/gt_version.R
@@ -0,0 +1,19 @@
+"gt_version" <- function(path=gt_path(), quiet=FALSE, all=FALSE) {
+  gt_files <- list.files(path, pattern="eruptions_data\\.rds$", recursive=TRUE)
+  versions <- as.Date(dirname(gt_files), format="%Y-%m-%d")
+  # only directories of form yyyy-mm-dd are allowed:
+  versions <- sort(versions[as.character(versions) == dirname(gt_files)],
+    decreasing=TRUE)
+  if(length(gt_files) < 1 || all(is.na(versions))) {
+    if(!quiet) {
+      message("Cannot find any GeyserTimes data under", path)
+    }
+    return(NULL)
+  }
+  version <- if(all) {
+    versions[!is.na(versions)]
+  } else {
+    versions[1]
+  }
+  version
+}
