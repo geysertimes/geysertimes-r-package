5627acd5620c193a81179146ee4d528c92c78a7e
spkaluzny <spkaluzny@gmail.com>
2019-05-10 17:02:17 -0700

    Removed geysertimes subdirectory

--- geysertimes/R/gt_path.R
+++
@@ -1,7 +0,0 @@
-"gt_path" <- function(temp=FALSE) {
-  if(temp) {
-    file.path(tempdir(), "GeyserTimes")
-  } else {
-    rappdirs::user_data_dir(appname = "GeyserTimes", appauthor = "GeyserTimes")
-  }
-}
